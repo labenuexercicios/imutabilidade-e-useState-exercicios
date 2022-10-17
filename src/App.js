8290e6aa55452703b1d6fc565342e6ff68920d29
GLCalegaro <giovaluiza@gmail.com>
2022-10-17 17:01:24 -0300

    Exercícios aula 34

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import styled, {createGlobalStyle} from 'styled-components'
 import PokemonCard from './Components/PokemonCard/PokemonCard';
+import { useStates } from 'react';
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -24,18 +25,45 @@ function App() {
     id: 0
   })
 
+  const [pokemon1, setPokemon1] = useState({
+    name: "Fennekin",
+    type: "Fire",
+    evolved: false,
+    weight: 9.4,
+    color: '#f05868',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/3/3d/653Fennekin.png/250px-653Fennekin.png',
+    id: 0
+  })
+
+  const [pokemon2, setPokemon2] = useState({
+    name: "Vulpix",
+    type: "Fire",
+    evolved: false,
+    weight: 9.9,
+    color: '#b07030',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/6/60/037Vulpix.png/250px-037Vulpix.png',
+    id: 0
+  })
   // Para fazer seus próximos pokemons, crie novos estados!
 
   return ( <>
   <GlobalStyles/>
     <FlexContainer>
       {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon}
+      setPokemon={setPokemon}
+      />
       {/* Crie aqui seus próximos pokemons! */}
+      <PokemonCard pokemon={pokemon1}
+      setPokemon={setPokemon1}
+      />
+      <PokemonCard pokemon={pokemon2}
+      setPokemon={setPokemon2}
+      />
     </FlexContainer>
   </>
     
-  );
+  )
 }
 
 export default App;
