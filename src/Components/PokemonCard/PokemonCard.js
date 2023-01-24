108b903b91c779f61e16b0eff42d45fc937d7e1e
Gabriel Garuthi <gabrielderu@hotmail.com>
2023-01-23 22:52:20 -0300

    Atualizado

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -7,15 +7,16 @@ const PokemonCard = ({nome, tipo,evolucao,peso,cor,imagem, id, setPokemon}) => {
 
     const evoluirPokemon = () => {
         console.log("Cliquei no botão de evoluir")
-        setPokemon({
-          nome: "Pikachu",
-          evolucao : true,
-          peso: 6,
-          imagem: 'https://archives.bulbagarden.net/media/upload/archive/4/4a/20100904002342%210025Pikachu.png'
-        })
-        console.log(nome)
 
-        console.log(evolucao)
+        if(evolucao !== true){
+          setPokemon({
+            name: "Pikachu",
+            evolved : true,
+            weight: 6,
+            image: 'https://archives.bulbagarden.net/media/upload/archive/4/4a/20100904002342%210025Pikachu.png',
+            id: 1
+          })
+        }
     }
     
   return (
@@ -25,7 +26,7 @@ const PokemonCard = ({nome, tipo,evolucao,peso,cor,imagem, id, setPokemon}) => {
         <PokemonType>{tipo}</PokemonType>
         <p>{peso}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+        <EvolveButton onClick={evoluirPokemon}>Evoluir!</EvolveButton>
     </Card>
   )
 }
