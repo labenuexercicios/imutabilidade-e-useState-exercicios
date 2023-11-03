892c812982aeee38325ddf186639587f83a480af
raissanfg <raissanfg@gmail.com>
2023-11-03 10:28:26 -0300

    Imutabilidade-exercicio-feito

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,20 +1,34 @@
 import React from 'react'
-import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
+import {Card, PokemonName, PokemonType, EvolveButton } from './styles'
 
 
 
-const PokemonCard = (props) => {
+
+const PokemonCard = ({figurinha, setFigurinha}) => {
 
     const evoluirPokemon = () => {
         console.log("Cliquei no botão de evoluir")
+        if (!figurinha.evolved) {
+          const novoPokemonEvoluido = {
+            ...figurinha,
+            name: "Pikachu Evoluído",
+            type: "Electric Evoluído",
+            evolved: true,
+          };
+          setFigurinha(novoPokemonEvoluido);
+        } else {
+          console.log("Pokémon já evoluiu!");
+        }
+      
+      
     }
-    
+
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={figurinha.color}>
+        <img src={figurinha.imagem} alt={`Pokemon`}/>
+        <PokemonName>{figurinha.name}</PokemonName>
+        <PokemonType>{figurinha.type}</PokemonType>
+        <p>{figurinha.weight}kg</p>
 
         <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
     </Card>
